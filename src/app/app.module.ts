3735fc8988a6b4efda325ac2aa6831925ccc28f3
hnunezz <nunesdealmeidahenrique@gmail.com>
2023-09-06 22:56:50 -0300

    --

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
+import { HeaderComponent } from './features/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  public texto: string = 'Login'
 
+  trocarNome() {
+    this.texto = 'Cadastrar'
+  }
 }
